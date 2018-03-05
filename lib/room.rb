f8b3a5ff8c91c073773d0a7f987ad528f4e7b56b
Zheng Cao <chori84@gmail.com>
2018-03-05 13:14:12 -0800

    Finish room_spec.rb, implement room.rb, and pass tests

--- lib/room.rb
+++ lib/room.rb
@@ -3,7 +3,12 @@ module Hotel
 
     attr_reader :number, :rate
 
-    def initialize
+    def initialize(number, rate)
+      if !(1..20).include?(number)
+        raise ArgumentError.new("Invalid room number")
+      end
+      @number = number
+      @rate = rate
     end
 
   end
--- specs/room_spec.rb
+++ specs/room_spec.rb
@@ -3,6 +3,29 @@ require_relative 'spec_helper'
 describe "Room" do
 
   describe "initialize" do
+    before do
+      @room = Hotel::Room.new(1, 200)
+    end
+
+    it "creates an instance of Room" do
+      @room.must_be_instance_of Hotel::Room
+    end
+
+    it "accesses the room number" do
+      @room.must_respond_to :number
+      @room.number.must_be_kind_of Integer
+      @room.number.must_equal 1
+    end
+
+    it "accesses the rate of the room" do
+      @room.must_respond_to :rate
+      @room.rate.must_be_instance_of Integer
+      @room.rate.must_equal 200
+    end
+
+    it "raises an ArgumentError if the room number is invalid" do
+      proc { room = Hotel::Room.new(21, 200) }.must_raise ArgumentError
+    end
 
   end
 
