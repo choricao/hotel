592ddbca17247d08aa02a3afa1a2c40093dbcaa7
Zheng Cao <chori84@gmail.com>
2018-03-05 12:57:23 -0800

    Outline reservation.rb and reservation_spec.rb

---
+++ lib/reservation.rb
@@ -0,0 +1,13 @@
+module Hotel
+  class Reservation
+
+    attr_reader :check_in_date, :check_out_date, :room, :cost
+
+    def initialize
+    end
+
+    def calculate_cost
+    end
+
+  end
+end
---
+++ specs/reservation_spec.rb
@@ -0,0 +1,13 @@
+require_relative 'spec_helper'
+
+describe "Reservation" do
+
+  describe "initialize" do
+
+  end
+
+  describe "calculate_cost" do
+
+  end
+
+end
