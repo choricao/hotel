992bf9709dfb38bee6c6a31bb7bfa4727c7d930d
Zheng Cao <chori84@gmail.com>
2018-03-05 12:53:46 -0800

    Outline front_desk.rb and front_desk_spec.rb

---
+++ lib/front_desk.rb
@@ -0,0 +1,16 @@
+module Hotel
+  class FrontDesk
+
+    attr_reader :rooms, :reservations
+
+    def initialize
+    end
+
+    def list_reservations(date)
+    end
+
+    def add_reservation(date_range)
+    end
+
+  end
+end
---
+++ specs/front_desk_spec.rb
@@ -0,0 +1,17 @@
+require_relative 'spec_helper'
+
+describe "FrontDesk" do
+
+  describe "initialize" do
+
+  end
+
+  describe "list_reservations" do
+
+  end
+
+  describe "add_reservation" do
+    
+  end
+
+end
